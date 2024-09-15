745bcd54393b7893ef7931aed2d7fdf424f033be
Mahagururaj <107253409+Mahagururaj@users.noreply.github.com>
2024-09-15 16:20:34 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+      '@': path.resolve(__dirname, 'src')
+    }
   },
   build: {
     rollupOptions: {
-      external: ['react-infinite-scroll-component'],
-    },
-  },
+      external: ['react-infinite-scroll-component']
+    }
+  }
 });
